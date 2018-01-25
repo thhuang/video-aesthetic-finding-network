34eb938390ee1f2f39e1d91ee8cafa92bff5857e
thhuang <huangTzu06>
2018-01-25 17:28:36 +0800

    Add model01.py

---
+++ scripts/model01.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+from keras.models import Sequential
+from keras.layers import ConvLSTM2D, LSTM, Dense, Dropout
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint
+from keras.utils import np_utils
+from datetime import datetime
+
+
+######################
+## Data Preparation ##
+######################
+
+
+#####################
+## Data Preprocess ##
+#####################
+
+
+####################
+## Initialization ##
+####################
+
+
+
