ddfb8669fe43000884574803fb951f7af2bca6f0
thhuang <huangTzu06>
2018-01-26 16:09:05 +0800

    Update

--- run_docker_druid.sh
+++ run_docker_druid.sh
@@ -3,6 +3,7 @@
 PRJ_ROOT='/data/thhuang/video-aesthetic-finding-network'
 INPUT_PATH='/data/thhuang/video-aesthetic-finding-network_input'
 OUTPUT_PATH='/data/thhuang/video-aesthetic-finding-network_output'
+DOWNLOADS_PATH='/data/thhuang/video-aesthetic-finding-network_downloads'
 IMAGE_NAME='thhuang/video-aesthetic-finding-network'
 PORT='3456'
 
@@ -10,5 +11,6 @@ nvidia-docker run -ti -p ${PORT}:8888 \
                   -v ${PRJ_ROOT}:/app \
                   -v ${INPUT_PATH}:/app/data/input:ro \
                   -v ${OUTPUT_PATH}:/app/data/output \
+                  -v ${DOWNLOADS_PATH}:/app/data/downloads \
                   ${IMAGE_NAME} bash
 
