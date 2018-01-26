104eb6a21a162d3c5c6797fb4afb04af0f2c36b2
thhuang <huangTzu06>
2018-01-26 15:48:57 +0800

    Update

--- scripts/evaluate_image_aesthetics_feature.py
+++ scripts/evaluate_image_aesthetics_feature.py
@@ -5,6 +5,7 @@ import argparse
 import tensorflow as tf
 import numpy as np
 import skimage.transform
+import skvideo.io
 import vfn.network as nw
 from vfn.vfn_eval import str2bool
 from utils.time_counters import time_counters
@@ -19,8 +20,9 @@ if __name__ == '__main__':
     parser.add_argument('--spp', help='Whether to use spatial pyramid pooling in the last layer or not', type=str, default='True')
     parser.add_argument('--pooling', help='Which pooling function to use', type=str, choices=['max', 'avg'], default='max')
     parser.add_argument('--input', help='Path to the file with images for aesthetics score evaluation', type=str, default='/app/data/input/images')
-    parser.add_argument('--input_video', help='Path to the video for aesthetics score evaluation', type=str)
-    parser.add_argument('--report_path', help='Path to score report', type=str, default='/app/data/output/images')
+    parser.add_argument('--input_video', help='Path to the video for aesthetics score evaluation', type=str, default='/app/data/input/videos/test.mp4')
+    parser.add_argument('--report_path', help='Path to score report', type=str, default='/app/data/output/videos')
+    parser.add_argument('--cpu_only', help='Use CPU only', type=str, default='True')
 
     args = parser.parse_args()
 
@@ -35,18 +37,20 @@ if __name__ == '__main__':
     images_dir = args.input
     video_dir = args.input_video
     report_dir = args.report_path
+    cpu_only = str2bool(args.cpu_only)
 
     with tf.variable_scope("ranker") as scope:
         feature_vec = nw.build_alexconvnet(image_placeholder, var_dict, embedding_dim, SPP=SPP, pooling=pooling)
         #score_func = nw.score(feature_vec)
         score_func = nw.score_1000(feature_vec)
 
-
-    # load pre-trained model
+    # Load pre-trained model
     t0, _ = time_counters()
     print('---load pre-trained model---')
     saver = tf.train.Saver(tf.global_variables())
-    sess = tf.Session(config=tf.ConfigProto(device_count = {'GPU': 0}))
+    if cpu_only:
+        config = tf.ConfigProto(device_count={'GPU': 0})
+    sess = tf.Session(config=config)
     sess.run(tf.global_variables_initializer())
     saver.restore(sess, snapshot)
     t0, _ = time_counters(t0, '>>> load pre-trained model', print_time=True)
