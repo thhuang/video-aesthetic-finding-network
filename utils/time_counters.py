c97a191bed3e5a6ea2bd68816a280b5963614c97
thhuang <huangTzu06>
2018-01-26 09:25:41 +0800

    Add time_counters.py

---
+++ utils/time_counters.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+def time_counters(start_time=None, task_name='', print_time=False):
+    if start_time == None:
+        return datetime.now(), None
+    else:
+        dt = datetime.now() - start_time
+        if print_time:
+            print(task_name + ':', dt)
+        return datetime.now(), dt
